99a0c1b65ab410e0f9e33ae8f1be2519f022c1dd
Rohan Tayal <109153434+rohantayal@users.noreply.github.com>
2024-06-21 12:15:28 +0530

    implemented fetch api

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ document.addEventListener("DOMContentLoaded",()=>{
     async function requestData(){
         const response = await fetch("../data.json");
         const data= await response.json();
-        console.log(data);
 
         btns.forEach( button => {
             button.addEventListener("click",()=>{
@@ -31,13 +30,21 @@ document.addEventListener("DOMContentLoaded",()=>{
                 });
 
                 const timeframe = document.querySelectorAll("."+dwm);
+                console.log(timeframe[0].children[0]);
                 const currentTime= document.querySelectorAll(".current_time");
-    
                 for(let i=0;i< data.length;i++)
                 {
-                    timeframe[i].children[0].innerHTML=data[i].timeframes[dwm].current;
-                    timeframe[i].children[1].innerHTML=data[i].timeframes[dwm].previous;
+                    timeframe[i].children[0].innerHTML=data[i].timeframes[dwm].current+ (data[i].timeframes[dwm].current>1?"hrs":"hr");
                     
+                    if(dwm=="daily"){
+                        timeframe[i].children[1].innerHTML="Yesterday - "+ data[i].timeframes[dwm].previous+(data[i].timeframes[dwm].previous>1?"hrs":"hr");
+                    }
+                    if(dwm=="weekly"){
+                        timeframe[i].children[1].innerHTML="Last week - "+ data[i].timeframes[dwm].previous+(data[i].timeframes[dwm].previous>1?"hrs":"hr");
+                    }
+                    if(dwm=="monthly"){
+                        timeframe[i].children[1].innerHTML="Last month - "+ data[i].timeframes[dwm].previous+(data[i].timeframes[dwm].previous>1?"hrs":"hr");
+                    }   
                 }
     
     
